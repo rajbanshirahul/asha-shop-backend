71b2abe85a76a8151fc9c3f6545fc567e581e1c5
rahulrajbanshi <link2rahulraj@gmail.com>
2021-05-27 13:56:03 +0545

    Initial project setup

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const { HOST, PORT, API_URL, CONNECTION_STRING } = process.env;
+
+// Middleware
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use(`${API_URL}/categories`, categoriesRoutes);
+app.use(`${API_URL}/products`, productsRoutes);
+app.use(`${API_URL}/users`, usersRoutes);
+app.use(`${API_URL}/orders`, ordersRoutes);
+
+mongoose
+  .connect(CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB..');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${HOST}:${PORT}`);
+});
